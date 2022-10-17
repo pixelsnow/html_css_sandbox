2e9e5460752db9edd15524bca08f791af6ab5b53
Vagapova Valeriia <lera.vagapova@gmail.com>
2022-10-18 00:58:48 +0300

    fix mobile menu

--- portfolio_new/main.js
+++ portfolio_new/main.js
@@ -2,7 +2,14 @@ const header = document.querySelector("header");
 const backButton = document.querySelector("#back_to_top");
 const mobButton = document.querySelector("#mob_button");
 const nav = document.querySelector(".main-menu");
+const menuItems = document.querySelector(".menu-items");
+const socials = document.querySelector(".socials");
 let links = document.querySelectorAll("nav ul li a");
+
+console.log("nav: ", nav);
+console.log("menuItems: ", menuItems);
+console.log("socials: ", socials);
+console.log("links: ", links);
 
 const scrollFunction = () => {
   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
@@ -25,14 +32,17 @@ const getToTop = () => {
 };
 
 const mobMenu = () => {
+  console.log("mobMenu called");
   for (const link of links) {
     link.addEventListener("click", mobMenu);
   }
   if (nav.classList.contains("responsive")) {
     nav.classList.remove("responsive");
+    /* socials.classList.remove("responsive"); */
     document.body.style.overflow = "";
   } else {
     nav.classList.add("responsive");
+    /* socials.classList.add("responsive"); */
     document.body.style.overflow = "hidden";
   }
 };
